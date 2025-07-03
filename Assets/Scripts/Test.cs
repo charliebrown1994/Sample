b9e1288f2a91ee819b5a9dda40d21e4a3a906621
charliebrown1994 <leesungyul@outlook.com>
2025-07-03 11:00:30 +0900

    25.07.03 11:00 commit

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public Text timeTxt;
-
-    float time = 0.0f;
-
     // Start is called before the first frame update
     void Start()
     {
+        // Hello, World를 Console 창에 출력한다.
+        // Debug.Log("Hello, World!");
 
+        // int age; // 변수를 선언한다 int는 박스고 age는 박스안에 물건이다.
+        // age = 30; // 변수에 값을 부여한다 "대입" 이라고 한다.
+        // Debug.Log(age); // console 창에 변수를 츨력한다.
+
+        // 변수에 변수를 대임한다
+        float height1 = 160.5f;
+        float height2;
+        height2 = height1; // height1을 height2에 대입한다.
+        Debug.Log(height2); // height2를 console 창에 출력한다.
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeTxt.text = time.ToString("N2");
+
     }
 }
